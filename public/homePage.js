c7d82562bb8aba26d8e793e9607fe6e63d74617c
Taut <nrdunaev@gmail.com>
2020-09-20 11:02:01 +0300

    Do bjs diploma

---
+++ public/homePage.js
@@ -0,0 +1,95 @@
+'use strict'
+
+const logoutButton = new LogoutButton();
+const ratesBoard = new RatesBoard();
+const moneyManager = new MoneyManager();
+const favoritesWidget = new FavoritesWidget();
+
+logoutButton.action = () => {
+    ApiConnector.logout(response => response.success ? location.reload() : moneyManager.setMessage(response.success, response.error));
+};
+
+ApiConnector.current(response => response.success ? ProfileWidget.showProfile(response.data) : moneyManager.setMessage(response.success, response.error));
+
+function exchangeRates() {
+	ApiConnector.getStocks(response => {
+        if (response.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        };
+    });
+};
+
+exchangeRates();
+setInterval(() => exchangeRates(), 60000);
+
+moneyManager.addMoneyCallback = data => {
+	ApiConnector.addMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Счет успешно пополнен!");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+		};
+	});
+};
+
+moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Сумма успешно конвертирована!");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        };
+    });
+};
+
+moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Сумма успешно переведена!");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        };
+    });
+};
+
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    } else {
+        favoritesWidget.setMessage(response.error);
+    };
+});
+
+favoritesWidget.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, "Пользователь успешно добавлен!");
+        } else {
+            favoritesWidget.setMessage(response.success, response.error)
+        };
+    });
+};
+
+favoritesWidget.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, "Пользователь успешно удален!");
+        } else {
+            favoritesWidget.setMessage(response.success, response.error)
+        };
+    });
+};
